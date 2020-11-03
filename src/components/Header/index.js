eccbaff75163c6ef2542b3bee463db5744d34ab8
PaulinaLL <paulinalll90@wp.pl>
2020-11-03 19:23:49 +0100

    create Header Component

---
+++ src/components/Header/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./index.css";
+import logo from "../../assets/logo.png";
+import contactSupport from "../../assets/Contact_Support.png";
+import contactSupportIcon from "../../assets/ic-contact_support.png";
+import burgerMenu from "../../assets/ic-menu_link.png";
+import icPower from "../../assets/ic-power_settings_new.png";
+import icUser from "../../assets/ic-user.png";
+import activeMessage from "../../assets/active.png";
+
+function Header() {
+  return (
+    <div className="header-wrapper">
+      <div className="logo-wrapper">
+        <img alt="burgerMenu" src={burgerMenu}></img>
+        <img alt="McMaclerLogo" src={logo}></img>
+      </div>
+      <div className="icons-wrapper">
+        <img alt="contactSupportLogo" src={contactSupportIcon}></img>
+        <img alt="contactSupportText" src={contactSupport}></img>
+
+        <img alt="activeMessage" src={activeMessage}></img>
+        <img alt="icUser" src={icUser}></img>
+        <img alt="icPower" src={icPower}></img>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
