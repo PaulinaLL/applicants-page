5429984655d4097e783d7aa17ed08a39a27d00b5
PaulinaLL <paulinalll90@wp.pl>
2020-11-04 14:27:41 +0100

    add fetching from an API with error handling and loading info

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import contacts from "./data/db.json";
-// import ContactsList from "./components/ContactsList/index";
 import Header from "./components/Header/index";
 import Dashboard from "./components/Dashboard/index";
 
+let api_url =
+  "https://raw.githubusercontent.com/PaulinaLL/mock-api/main/db.json";
+
 function App() {
-  return (
-    <div className="App">
-      <Header contacts={contacts} />
-      <Dashboard contacts={contacts} />
-      <ul>
-        <li>AGB - </li>
-        <li> Datenschutz - </li>
-        <li> Impressum</li>
-      </ul>
-      {/* 
-      <ContactsList contacts={contacts} /> */}
-    </div>
-  );
+  let [data, setData] = useState([]);
+  let [isLoaded, setIsLoaded] = useState(false);
+  let [err, setErr] = useState(null);
+  useEffect(() => {
+    const getData = () => {
+      fetch(api_url)
+        .then((res) => {
+          if (res.status >= 400) {
+            throw new Error("Upss something went wrong (server errror)!");
+          }
+          return res.json();
+        })
+        .then(
+          (data) => {
+            setData(data);
+            setIsLoaded(true);
+          },
+          (err) => {
+            setErr(err);
+            setIsLoaded(true);
+          }
+        );
+    };
+    getData();
+  }, []);
+
+  if (err) {
+    return <div> {err.message} </div>;
+  } else if (!isLoaded) {
+    return <div> Loading... </div>;
+  } else {
+    return (
+      <div className="App">
+        <Header contacts={data} />
+        <Dashboard contacts={data} />
+        <ul>
+          <li>AGB - </li>
+          <li> Datenschutz - </li>
+          <li> Impressum</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
