5475bb59e6dc46b32a40f119947f844767e2b833
PaulinaLL <paulinalll90@wp.pl>
2020-11-04 11:13:29 +0100

    improve logic in display stats

--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -5,22 +5,28 @@ function ContactsList(props) {
   console.log("here", props.stats);
   return (
     <div>
-      <h4>Appointment set({props.stats.appointment})</h4>
+      <h4>
+        Appointment set({props.stats.appointment ? props.stats.appointment : 0})
+      </h4>
 
       <ul className="card-list">
         {props.data.contacts.applicants.map((item) => {
           return <ContactsListItem key={item.id} item={item} />;
         })}
       </ul>
-      <h4>Property viewed({props.stats.viewed})</h4>
+      <h4>Property viewed( {props.stats.viewed ? props.stats.viewed : 0})</h4>
 
       <ul className="card-list">
         {props.data.contacts.applicants.map((item) => {
           return <ContactsListItem key={item.id} item={item} />;
         })}
       </ul>
-      <h4>Interested({props.stats.viewed})</h4>
-      <h4>Offer accepted({props.stats.offerAccepted})</h4>
+      <h4>Interested({props.stats.interested ? props.stats.interested : 0})</h4>
+
+      <h4>
+        Offer accepted(
+        {props.stats.offerAccepted ? props.stats.offerAccepted : 0})
+      </h4>
     </div>
   );
 }
