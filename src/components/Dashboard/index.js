bf16ee2d0965d8c7147c0a28847183554fe17499
PaulinaLL <paulinalll90@wp.pl>
2020-11-04 12:25:34 +0100

    change if else logic & add filter function by name,last name,email, add arrow

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,38 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 import SearchAndFilter from "../SearchAndFilter/index";
 import InfoList from "../InfoList/index";
 import ContactList from "../ContactsList/index";
+import arrow from "../../assets/arrow.png";
 
 function Dashboard(props) {
+  const [searchField, setSearchFieldState] = useState("");
+
   const total = props.contacts.applicants.length;
   const arrayOfContactsInObject = props.contacts.applicants;
 
   const groupInfo = arrayOfContactsInObject.reduce((groups, person) => {
-    const { viewed = 0, appointment = 0, newCategory = 0, others = 0 } = groups;
-    if (person.viewed !== "") {
+    const {
+      viewed = 0,
+      appointment = 0,
+      newCategory = 0,
+      offerAccepted = 0,
+      others = 0,
+    } = groups;
+    if (person.offerAccepted !== "") {
+      return { ...groups, offerAccepted: offerAccepted + 1 };
+    } else if (person.viewed !== "") {
       return { ...groups, viewed: viewed + 1 };
     } else if (person.appointment !== "") {
       return { ...groups, appointment: appointment + 1 };
-    } else if (person.interested === "" && person.appointment === "") {
-      return { ...groups, others: others + 1 };
+    } else if (person.interested !== "" && person.appointment === "") {
+      return { ...groups, newCategory: newCategory + 1 };
     } else {
-      return { ...groups, new: newCategory + 1 };
+      return { ...groups, others: others + 1 };
     }
   }, {});
 
   console.log(groupInfo);
-
+  const filteredCandidates = arrayOfContactsInObject.filter(
+    (candidate) =>
+      candidate.firstName.toLowerCase().includes(searchField.toLowerCase()) ||
+      candidate.lastName.toLowerCase().includes(searchField.toLowerCase()) ||
+      candidate.email.toLowerCase().includes(searchField.toLowerCase())
+  );
+  console.log(filteredCandidates);
   return (
     <div className="wrapper">
       <div className="info-section">
+        <img alt="arrow-left" src={arrow} height="16px"></img>
         <h3>Applicants</h3>
         <div className="info-wrapper">
           <InfoList stats={groupInfo} total={total} />
         </div>
       </div>
-      <SearchAndFilter />
-      <ContactList stats={groupInfo} data={props} />
+      <SearchAndFilter
+        handleChange={(e) => setSearchFieldState(e.target.value)}
+      />
+      <ContactList stats={groupInfo} data={filteredCandidates} />
     </div>
   );
 }
