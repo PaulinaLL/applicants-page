9f9adeddb5195105eaa60f7f8230c707feaa6071
PaulinaLL <paulinalll90@wp.pl>
2020-11-05 21:58:57 +0100

    clean up, simplfy filter

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -8,37 +8,34 @@ import arrow from "../../assets/back.svg";
 import queryString from "query-string";
 
 function Dashboard(props) {
-  // const [searchField, setSearchFieldState] = useState("");
   const total = props.contacts.applicants.length;
   const arrayOfContactsInObject = props.contacts.applicants;
   const parsed = queryString.parse(props.location.search);
   const searchWord = parsed.search;
-  console.log(searchWord);
 
   // group info to display stats
   const groupInfo = arrayOfContactsInObject.reduce((groups, person) => {
     const {
       viewed = 0,
       appointment = 0,
-      newCategory = 0,
+      interested = 0,
       offerAccepted = 0,
       others = 0,
     } = groups;
-    if (person.offerAccepted !== "") {
+    if (person.interested !== "") {
+      return { ...groups, interested: interested + 1 };
+    } else if (person.offerAccepted !== "") {
       return { ...groups, offerAccepted: offerAccepted + 1 };
     } else if (person.viewed !== "") {
       return { ...groups, viewed: viewed + 1 };
     } else if (person.appointment !== "") {
       return { ...groups, appointment: appointment + 1 };
-    } else if (person.interested !== "" && person.appointment === "") {
-      return { ...groups, newCategory: newCategory + 1 };
     } else {
       return { ...groups, others: others + 1 };
     }
   }, {});
 
   const updateSearch = (searchString) => {
-    // setSearchFieldState(searchString);
     props.history.push(`/?search=${searchString}`);
   };
 
